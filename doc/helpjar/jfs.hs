8c644f0fad58f1de79b2c7bdf98cb2cb2b368f8c
skangas <skangas@bfc8530a-dd38-4376-9a49-ec652ef1b1b4>
2010-04-08 21:28:24 +0000

    Extract 'doc/help.jar' to 'doc/helpjar', removing the original file
    git-svn-id: svn://svn.code.sf.net/p/jfsaccounting/code/trunk@160 bfc8530a-dd38-4376-9a49-ec652ef1b1b4

---
+++ doc/helpjar/jfs.hs
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<helpset>
+  <title>JFS Administration hjälp</title>
+  <maps>
+    <homeID>hem_html</homeID>
+    <mapref location="default.map"/>
+  </maps>
+  <view>
+    <name>Table of Contents</name>
+    <label>Innehåll</label>
+    <image>New Text Document 16 n p_png</image>
+    <type>javax.help.TOCView</type>
+    <data>toc.xml</data>
+  </view>
+  <view>
+    <name>Search</name>
+    <label>Sök</label>
+    <image>Search 16 n p_png</image>
+    <type>javax.help.SearchView</type>
+    <data engine="com.sun.java.help.search.DefaultSearchEngine">JavaHelpSearch</data>
+  </view>
+  <presentation>
+    <name>Administration presentation</name>
+  </presentation>
+</helpset>
