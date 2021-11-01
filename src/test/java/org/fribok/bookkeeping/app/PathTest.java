eabbb05d834a4b418e7d205416cea973f89cdd2d
ellefj <ellefj@bfc8530a-dd38-4376-9a49-ec652ef1b1b4>
2021-11-01 18:17:46 +0000

    [housecleaning] And moving the test for the post 2008 app-package from se.swedsoft to org.fribok to reflect the origin correctly
    git-svn-id: svn://svn.code.sf.net/p/jfsaccounting/code/trunk@238 bfc8530a-dd38-4376-9a49-ec652ef1b1b4

--- src/test/java/org/fribok/bookkeeping/app/PathTest.java
+++ src/test/java/org/fribok/bookkeeping/app/PathTest.java
@@ -16,12 +16,12 @@
  * You should have received a copy of the GNU General Public License along with
  * Fribok.  If not, see <http://www.gnu.org/licenses/>.
  */
-package se.swedsoft.bookkeeping.app;
+package org.fribok.bookkeeping.app;
 
 
 import org.junit.Assert;
 import org.junit.Test;
-import se.swedsoft.bookkeeping.app.Path;
+import org.fribok.bookkeeping.app.Path;
 
 
 /**
